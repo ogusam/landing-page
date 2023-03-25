552d1640862e70d470f6baaf903a97e7cf7c12fb
ogusam <ogusam32@gmail.com>
2023-03-25 02:12:15 +0100

    progress

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router =express.Router();
 const User =require("../models/User");
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const ValidateRegisterInput = require("../validation/registerValidation");
 
 router.get("/test", (req, res)=>{
     res.send("Auth route working");
@@ -9,11 +10,28 @@ router.get("/test", (req, res)=>{
 
 router.post("/register", async (req, res) =>{
     try{
+        const { password } = req.body
+  if (password.length < 6) {
+    return res.status(400).json({ message: "Password less than 6 characters" })
+  };
+  const {errors, isValid} = ValidateRegisterInput(req.body);
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+        //check for existing user
+        const existingEmail =await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i")
+        });
+        if(existingEmail){
+            return res.status(400)
+            .json({erro: "There is already a user with same email"});
+        };
+        
         const hashedPassword = await bcrypt.hash(req.body.password, 12);
         const newUser = new User({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
-            password: req.body.password,
+            password: hashedPassword,
             email: req.body.email
         });
 
---
+++ server/validation/registerValidation.js
@@ -0,0 +1,43 @@
+const Validator = require("Validator");
+const isEmpty = require('./isEmpty');
+
+
+
+
+
+const ValidateRegisterInput = (data) =>{
+   let errors ={};
+
+   if(isEmpty(data.firstName)){
+    errors.firstName = "This field is required";
+   }
+   if(isEmpty(data.lastName)){
+    errors.lastName="this field is required";
+   }
+
+   if(isEmpty(data.email)){
+       errors.email = "This field is required";
+   }else if(!Validator.isEmail(data.email)) {
+       errors.email = "Email is invalid";
+   }
+
+   if (isEmpty(data.password)) {
+       errors.password = " password required";
+   } 
+   
+   
+
+   //check confirm password
+   if (isEmpty(data.confirmPassword)) {
+       errors.confirmPassword = "This field is empty";
+   } else if (!Validator.equals(data.password, data.confirmPassword)) 
+   {errors.confirmPassword = "password and confirm password must match";
+       
+   }
+   return {
+       errors,
+       isValid:isEmpty(errors),
+   }
+};
+
+ module.exports = ValidateRegisterInput;
